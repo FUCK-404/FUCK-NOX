bb24f7a05cd5cb92b2adb99aa6845384f923d127
FUCK-404 <109244698+FUCK-404@users.noreply.github.com>
2022-10-29 03:25:48 +0600

    Update FUCK-NOX.py

--- FUCK-NOX.py
+++ FUCK-NOX.py
@@ -12,13 +12,4 @@ os.system('xdg-open https://www.facebook.com/tera.crush.69k')
 if machine()=='aarch64':
     import nox
     nox.main()
-else:
-    import nox32
-    nox32.main()
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import nox
-    nox.main
-elif bit == '32bit':
-    import nox32
-    nox32.main()
+
