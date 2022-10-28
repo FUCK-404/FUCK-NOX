ea0c25f37ec94a5b4b51c50acddf63f208d2b7d7
FUCK-404 <109244698+FUCK-404@users.noreply.github.com>
2022-10-29 02:28:34 +0600

    Create Nox.py

---
+++ Nox.py
@@ -0,0 +1,18 @@
+from os import system
+import time
+from platform import machine
+print('Checking For Update...')
+system('git pull')
+print('Subcrib My Channel First...')
+os.system('xdg-open https://youtube.com/c/MrQureshiTech')
+time.sleep(2)
+print('Join Our Facebook Group For More Update bruh...')
+os.system('xdg-open https://facebook.com/groups/447671328737321/')
+if machine()=='aarch64':
+    
+    import nox.so
+    nox.main
+else:
+    
+    import nox32.so
+    nox.main
